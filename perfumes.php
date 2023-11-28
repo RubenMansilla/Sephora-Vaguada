0221a755331f76db4ccb500a7d8da34221e5d92a
kiwan33 <rumansillar@gmail.com>
2023-11-28 18:02:50 +0100

    styles added

--- perfumes.php
+++ perfumes.php
@@ -6,6 +6,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="./styles/normalize.css">
     <link rel="stylesheet" href="./styles/global.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="styles/perfume.css">
 </head>
 <body>
 
@@ -13,7 +14,7 @@
         include './includes/header.php'; 
         include './includes/nav.php';
     ?>
-
+    <video id="video" loop="loop" autoplay="autoplay" title="Xmas 2023" playsinline="" crossorigin="anonymous" src="https://customer-jsjst1fdhj7sipkx.cloudflarestream.com/1958fdfffd097a3a52d42b7f8269f13e/thumbnails/thumbnail.mp4?&amp;width=1080&amp;duration=12s"></video>
     <section></section>
 
     <?php 
