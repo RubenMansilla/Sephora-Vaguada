15398bb210828178f7f3121435a5d3491b47ec4a
kiwan33 <rumansillar@gmail.com>
2023-11-29 17:12:52 +0100

    aside make-up

--- maquillaje.php
+++ maquillaje.php
@@ -6,6 +6,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="./styles/normalize.css">
     <link rel="stylesheet" href="./styles/global.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="./styles/maquillaje.css?v=<?php echo time(); ?>">
 </head>
 <body>
     
@@ -13,6 +14,29 @@
         include './includes/header.php'; 
         include './includes/nav.php';
     ?>
+
+    <h1>Maquillaje</h1>
+
+    <aside class="descuentoMaquillaje">
+        <img>
+        <div class="cajaTexto">
+            <p class="texto1">MAQUILLAJE</p>
+            <p class="texto2">DE LUJO</p>
+        </div>
+        <div class="cajaDescuento">
+            <p>50%</p>
+        </div>
+    </aside>
+    <aside class="descuentoMaquillaje">
+        <img>
+        <div class="cajaTexto">
+            <p class="texto1">MAQUILLAJE</p>
+            <p class="texto2">LOW-COST</p>
+        </div>
+        <div class="cajaDescuento">
+            <p class="descuento">20%</p>
+        </div>
+    </aside>
 
     <section></section>
 
