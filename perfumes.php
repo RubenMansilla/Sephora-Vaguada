f3ea4f591589935b393d4aa4e79dc9d3de66f190
kiwan33 <rumansillar@gmail.com>
2023-11-29 11:18:37 +0100

    products added

--- perfumes.php
+++ perfumes.php
@@ -23,19 +23,19 @@
     <p class="subTitulo">Sumérgete en el mundo de las fragancias con nuestra colección de perfumes, cuidado personal e ideas para regalos.</p>
 
     <div class="tipos">
-        <img src="img/conjuntoDePerfumes.jpg" alt="Perfumes populares">
+        <img src="img/perfumes/conjuntoDePerfumes.jpg" alt="Perfumes populares">
         <a href="#populares"><p>Populares</p></a>
     </div>
     <div class="tipos">
-        <img src="img/perfumeMujer.jpg" alt="Perfumes Mujer">
+        <img src="img/perfumes/perfumeMujer.jpg" alt="Perfumes Mujer">
         <a href="#mujer"><p>Perfumes Mujer</p></a>
     </div>
     <div class="tipos">
-        <img src="img/perfumeHombre.jpg" alt="Perfumes Hombre">
-        <p>Perfumes Hombre</p>
+        <img src="img/perfumes/perfumeHombre.jpg" alt="Perfumes Hombre">
+        <a href="#hombre"><p>Perfumes Hombre</p></a>
     </div>
     <div class="tipos">
-        <img src="img/SetRegaloPerfumes.jpg" alt="Set Regalo de Perfumes">
+        <img src="img/perfumes/SetRegaloPerfumes.jpg" alt="Set Regalo de Perfumes">
         <p>Cofres Regalo</p>
     </div>
 
@@ -48,120 +48,184 @@
     <section class="tipoProductos">
         <h2><a id="populares"></a>Populares</h2>
         <div class="producto">
-            <img class="imgProducto1" src="img/Armani My Way Eau de Parfum - 30ml.png" alt="Armani My Way Eau de Parfum - 30ml">
+            <img class="imgProducto1" src="img/perfumes/Armani My Way Eau de Parfum - 30ml.png" alt="Armani My Way Eau de Parfum - 30ml">
             <p class="nombreProducto">Giorgio Armani My Way Eau de Parfum - 30ml</p>
-            <p class="precioProducto"><strong>52,95€</strong></p>
+            <p class="precioProducto">52,95 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto1" src="img/viktor-rolf-spicebomb-infrared-eau-de-toilette.png" alt="Viktor & Rolf Spicebomb Infrared Eau de Toilette - 50ml">
+            <img class="imgProducto1" src="img/perfumes/viktor-rolf-spicebomb-infrared-eau-de-toilette.png" alt="Viktor & Rolf Spicebomb Infrared Eau de Toilette - 50ml">
             <p class="nombreProducto">Viktor & Rolf Spicebomb Infrared Eau de Toilette - 50ml</p>
-            <p class="precioProducto">75,45€</p>
+            <p class="precioProducto">75,45 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto2" src="img/Perfect Marc Jacobs Eau de Parfum 100ml.png" alt="Perfect Marc Jacobs Eau de Parfum 100ml">
+            <img class="imgProducto2" src="img/perfumes/Perfect Marc Jacobs Eau de Parfum 100ml.png" alt="Perfect Marc Jacobs Eau de Parfum 100ml">
             <p class="nombreProducto">Perfect Marc Jacobs Eau de Parfum 100ml</p>
-            <p class="precioProducto">92,95€</p>
+            <p class="precioProducto">92,95 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto1" src="img/Good Girl 100ml Eau de Parfum Refillable Edition.png" alt="Good Girl 100ml Eau de Parfum Refillable Edition">
+            <img class="imgProducto1" src="img/perfumes/Good Girl 100ml Eau de Parfum Refillable Edition.png" alt="Good Girl 100ml Eau de Parfum Refillable Edition">
             <p class="nombreProducto">Good Girl 100ml Eau de Parfum Refillable Edition</p>
-            <p class="precioProducto">70,00€</p>
+            <p class="precioProducto">70,00 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto3" src="img/Calendario de adviento Herrera 12 días de belleza.png" alt="Calendario de adviento Herrera: 12 días de belleza">
+            <img class="imgProducto3" src="img/perfumes/Calendario de adviento Herrera 12 días de belleza.png" alt="Calendario de adviento Herrera: 12 días de belleza">
             <p class="nombreProducto">Calendario de adviento Herrera: 12 días de belleza</p>
-            <p class="precioProducto">99,50€</p>
+            <p class="precioProducto">99,50 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto" >
-            <img class="imgProducto4" src="img/Yves Saint Laurent MYSLF Eau de Parfum 60ml.png" alt="Yves Saint Laurent MYSLF Eau de Parfum 60ml">
+            <img class="imgProducto4" src="img/perfumes/Yves Saint Laurent MYSLF Eau de Parfum 60ml.png" alt="Yves Saint Laurent MYSLF Eau de Parfum 60ml">
             <p class="nombreProducto">Yves Saint Laurent MYSLF Eau de Parfum 60ml</p>
-            <p class="precioProducto">82,95€</p>
+            <p class="precioProducto">82,95 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto5" src="img/MUGLER Alien Eau de Parfum Natural Spray Recargable - 90ml.png" alt="MUGLER Alien Eau de Parfum Natural Spray Recargable - 90ml">
-            <p class="nombreProducto">MUGLER Alien Eau de Parfum Natural Spray Recargable - 90ml</p>
-            <p class="precioProducto">116,45€</p>
+            <img class="imgProducto5" src="img/perfumes/MUGLER Alien Eau de Parfum Natural Spray Recargable - 90ml.png" alt="MUGLER Alien Eau de Parfum Natural Spray Recargable - 90ml">
+            <p class="nombreProducto">MUGLER Alien Eau de Parfum Natural Spray - 90ml</p>
+            <p class="precioProducto">116,45 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto6" src="img/Versace Eros for Men Eau de Toilette 100ml.png" alt="Versace Eros for Men Eau de Toilette 100ml">
+            <img class="imgProducto6" src="img/perfumes/Versace Eros for Men Eau de Toilette 100ml.png" alt="Versace Eros for Men Eau de Toilette 100ml">
             <p class="nombreProducto">Versace Eros for Men Eau de Toilette 100ml</p>
-            <p class="precioProducto">96,45€</p>
+            <p class="precioProducto">96,45 €</p>
+            <p class="botonCompra">Comprar</p>
+        </div>
+    </section>
+
+    <aside>
+        <h2 class="tituloAside">BLACK FRIDAY: 10% EXTRA</h2>
+        <img class="imgAside" src="img/perfumes/venta-perfumes-black-friday_66941-92.png" alt="">
+        <p class="textoAside">¡Aprovecha hasta un 50% de descuento en compras + 10% EXTRA en tus porductos favoritos!<br><br>¡Elige un REGALO para ti o para alguién especial con compras superiores a 100€!<br><br>Oferta disponuble hasta el 06/01/2024</p>
+    </aside>
+
+    <section class="tipoProductos">
+        <h2><a id="mujer"></a>Perfumes Mujer</h2>
+        <div class="producto">
+            <img class="imgProducto4" src="Img/perfumes/Eau de Rochas L'essentiel - Eau de Parfum.png" alt="Eau de Rochas L'essentiel - Eau de Parfum">
+            <p class="nombreProducto">Eau de Rochas L'essentiel - Eau de Parfum</p>
+            <p class="precioProducto">69,99 €</p>
+            <p class="botonCompra">Comprar</p>
+        </div>
+
+        <div class="producto">
+            <img class="imgProducto3" src="img/perfumes/Burberry Goddess Eau de Parfum for Women 100ml.jpg" alt="Burberry Goddess Eau de Parfum for Women 100ml">
+            <p class="nombreProducto">Burberry Goddess Eau de Parfum for Women 100ml</p>
+            <p class="precioProducto">113,45 €</p>
+            <p class="botonCompra">Comprar</p>
+        </div>
+
+        <div class="producto">
+            <img class="imgProducto2" src="img/perfumes/Ode To Dullness - Eau de Parfum.png" alt="Ode To Dullness - Eau de Parfum">
+            <p class="nombreProducto">Ode To Dullness - Eau de Parfum</p>
+            <p class="precioProducto">125,99 €</p>
+            <p class="botonCompra">Comprar</p>
+        </div>
+
+        <div class="producto">
+            <img class="imgProducto3" src="img/perfumes/Yves Saint Laurent Black Opium Eau de Parfum 30ml.png" alt="Yves Saint Laurent Black Opium Eau de Parfum 30ml">
+            <p class="nombreProducto">Yves Saint Laurent Black Opium Eau de Parfum 30ml</p>
+            <p class="precioProducto">52,95 €</p>
+            <p class="botonCompra">Comprar</p>
+        </div>
+
+        <div class="producto">
+            <img class="imgProducto2" src="img/perfumes/Alien Goddess Supra Florale - Eau de Parfum.png" alt="Alien Goddess Supra Florale - Eau de Parfum">
+            <p class="nombreProducto">Alien Goddess Supra Florale - Eau de Parfum</p>
+            <p class="precioProducto">104,99 €</p>
+            <p class="botonCompra">Comprar</p>
+        </div>
+
+        <div class="producto" >
+            <img class="imgProducto4" src="img/perfumes/Bergamot Bloom 100ml Eau de Toilette.png" alt="Bergamot Bloom 100ml Eau de Toilette">
+            <p class="nombreProducto">Bergamot Bloom 100ml Eau de Toilette</p>
+            <p class="precioProducto">125,00 €</p>
+            <p class="botonCompra">Comprar</p>
+        </div>
+
+        <div class="producto">
+            <img class="imgProducto5" src="img/perfumes/Yves Saint Laurent Libre Eau de Parfum 90ml.jpg" alt="Yves Saint Laurent Libre Eau de Parfum 90ml">
+            <p class="nombreProducto">Yves Saint Laurent Libre Eau de Parfum 90ml</p>
+            <p class="precioProducto">101,95 €</p>
+            <p class="botonCompra">Comprar</p>
+        </div>
+
+        <div class="producto">
+            <img class="imgProducto4" src="img/perfumes/More More Pink - Eau de Toilette.jpg" alt="More More Pink - Eau de Toilette">
+            <p class="nombreProducto">More More Pink - Eau de Toilette</p>
+            <p class="precioProducto">89,99 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
     </section>
 
     <section class="tipoProductos">
-        <h2><a id="mujer"></a>Perfumes Mujer</h2>
+        <h2><a id="hombre"></a>Perfumes Hombre</h2>
         <div class="producto">
-            <img class="imgProducto7" src="Img/Eau de Rochas L'essentiel - Eau de Parfum.png" alt="Eau de Rochas L'essentiel - Eau de Parfum">
-            <p class="nombreProducto">Eau de Rochas L'essentiel - Eau de Parfum</p>
-            <p class="precioProducto"><strong>69,99€</strong></p>
+            <img class="imgProducto4" src="img/perfumes/BOSS Bottled Elixir - Fragancia intensa.png" alt="BOSS Bottled Elixir - Fragancia intensa">
+            <p class="nombreProducto">BOSS Bottled Elixir - Fragancia intensa</p>
+            <p class="precioProducto">109,99 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto3" src="img/Burberry Goddess Eau de Parfum for Women 100ml.jpg" alt="Burberry Goddess Eau de Parfum for Women 100ml">
-            <p class="nombreProducto">Burberry Goddess Eau de Parfum for Women 100ml</p>
-            <p class="precioProducto">113,45€</p>
+            <img class="imgProducto4" src="img/perfumes/Bad Boy 150ml Eau de Toilette Refillable Edition.png" alt="Bad Boy 150ml Eau de Toilette Refillable Edition">
+            <p class="nombreProducto">Bad Boy 150ml Eau de Toilette Refillable Edition</p>
+            <p class="precioProducto">137,50 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto2" src="img/Ode To Dullness - Eau de Parfum.png" alt="Ode To Dullness - Eau de Parfum">
-            <p class="nombreProducto">Ode To Dullness - Eau de Parfum</p>
-            <p class="precioProducto">125,99€</p>
+            <img class="imgProducto1" src="img/perfumes/Paco Rabanne Invictus Eau de Toilette 200ml.png" alt="Paco Rabanne Invictus Eau de Toilette 200ml">
+            <p class="nombreProducto">Paco Rabanne Invictus Eau de Toilette 200ml</p>
+            <p class="precioProducto">105,95 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto3" src="img/Yves Saint Laurent Black Opium Eau de Parfum 30ml.png" alt="Yves Saint Laurent Black Opium Eau de Parfum 30ml">
-            <p class="nombreProducto">Yves Saint Laurent Black Opium Eau de Parfum 30ml</p>
-            <p class="precioProducto">52,95€</p>
+            <img class="imgProducto5" src="img/perfumes/Jean Paul Gaultier Le Male Agua de Colonia 125ml.png" alt="Jean Paul Gaultier Le Male Agua de Colonia 125ml">
+            <p class="nombreProducto">Jean Paul Gaultier Le Male Agua de Colonia 125ml</p>
+            <p class="precioProducto">77,45 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto2" src="img/Alien Goddess Supra Florale - Eau de Parfum.png" alt="Alien Goddess Supra Florale - Eau de Parfum">
-            <p class="nombreProducto">Alien Goddess Supra Florale - Eau de Parfum</p>
-            <p class="precioProducto">104,99€</p>
+            <img class="imgProducto1" src="img/perfumes/Valentino Born in Roma Uomo Eau de Toilette for Him 100ml.png" alt="Valentino Born in Roma Uomo Eau de Toilette for Him 100ml">
+            <p class="nombreProducto">Valentino Born in Roma Uomo Eau de Toilette for Him 100ml</p>
+            <p class="precioProducto">73,45 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto" >
-            <img class="imgProducto4" src="img/Bergamot Bloom 100ml Eau de Toilette.png" alt="Bergamot Bloom 100ml Eau de Toilette">
-            <p class="nombreProducto">Bergamot Bloom 100ml Eau de Toilette</p>
-            <p class="precioProducto">125,00€</p>
+            <img class="imgProducto3" src="img/perfumes/Prada Luna Rossa Carbon Eau de Toilette - 100ml.png" alt="Prada Luna Rossa Carbon Eau de Toilette - 100ml">
+            <p class="nombreProducto">Prada Luna Rossa Carbon Eau de Toilette - 100ml</p>
+            <p class="precioProducto">78,95 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto5" src="" alt="">
-            <p class="nombreProducto"></p>
-            <p class="precioProducto"></p>
+            <img class="imgProducto1" src="img/perfumes/Armani Acqua Di Gio Homme Eau de Toilette - 100ml.png" alt="Armani Acqua Di Gio Homme Eau de Toilette - 100ml">
+            <p class="nombreProducto">Armani Acqua Di Gio Homme Eau de Toilette - 100ml</p>
+            <p class="precioProducto">72,45 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
 
         <div class="producto">
-            <img class="imgProducto6" src="" alt="">
-            <p class="nombreProducto"></p>
-            <p class="precioProducto"></p>
+            <img class="imgProducto3" src="img/perfumes/Burberry Hero Eau de Parfum for Men 50ml.png" alt="Burberry Hero Eau de Parfum for Men 50ml">
+            <p class="nombreProducto">Burberry Hero Eau de Parfum for Men 50ml</p>
+            <p class="precioProducto">67,95 €</p>
             <p class="botonCompra">Comprar</p>
         </div>
-
     </section>
 
     <?php
